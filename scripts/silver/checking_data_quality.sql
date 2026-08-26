79ee47b17187f20445ba100ce6dc8872db7a0966
Zwe Pyi Phyo <99266799+ZwePP@users.noreply.github.com>
2026-08-26 15:25:37 +0700

    Create checking_data_quality.sql for data validation
    Added SQL script to check data quality for customer information.

---
+++ scripts/silver/checking_data_quality.sql
@@ -0,0 +1,28 @@
+-- Check for nulls or duplicates in Primary Key
+-- Expectation: No result
+SELECT cst_id, COUNT(*) FROM bronze.crm_cust_info
+GROUP BY cst_id HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- CHECK FOR UNWATED SPACES
+-- Expectation: No Results
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+SELECT cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr);
+
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info;
+
+-- Data Standardization & Consistency
+SELECT DISTINCT cst_martial
+FROM bronze.crm_cust_info;
+
