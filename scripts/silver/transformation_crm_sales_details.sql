9adc230b433902b801d5216876f260c8455bbd19
Zwe Pyi Phyo <99266799+ZwePP@users.noreply.github.com>
2026-08-28 10:46:44 +0700

    Update transformation_crm_sales_details.sql

--- scripts/silver/transformation_crm_sales_details.sql
+++ scripts/silver/transformation_crm_sales_details.sql
@@ -1,3 +1,14 @@
+INSERT INTO silver.crm_sales_details(
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+)
 SELECT
 	sls_ord_num,
 	sls_prd_key,
@@ -11,15 +22,15 @@ SELECT
 	CASE WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::VARCHAR) != 8 THEN NULL
 		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
 	END AS sls_due_dt,
-	sls_sales,
+	CASE WHEN sls_sales IS NULL 
+		OR sls_sales <= 0 
+		OR sls_sales != sls_quantity * ABS(sls_price)
+		THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS sls_sales,
 	sls_quantity,
-	sls_price
-FROM bronze.crm_sales_details
-
-SELECT NULLIF(sls_order_dt,0) sls_order_dt
-FROM bronze.crm_sales_details
-WHERE sls_order_dt <= 0
-OR LENGTH(sls_order_dt::VARCHAR) != 8;
-
-SELECT * FROM bronze.crm_sales_details
-WHERE sls_order_dt > sls_ship_dt;
+	CASE WHEN sls_price IS NULL OR sls_price <= 0
+		THEN sls_sales / NULLIF(sls_quantity,0)
+		ELSE sls_price
+	END AS sls_price
+FROM bronze.crm_sales_details;
